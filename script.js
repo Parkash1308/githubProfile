52daffed06a96f24d342aabbbfadcabef2f3608e
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-03-09 22:35:48 +0500

    create webpage to get github profile

---
+++ script.js
@@ -0,0 +1,13 @@
+const sub= document.getElementById("submit")
+
+
+function addProfile(){
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+    const github = document.getElementById("account").value
+    console.log(name, email, github)
+    const profile= document.createElement("div")
+
+}
+
+sub.addProfile
